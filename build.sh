46e88103ce8416bc24a59540e7c20051f6bdd838
Mark J. Olah <markjolah@users.noreply.github.com>
2017-06-23 22:19:57 -0600

    Seperate PackageConfig functionality into module

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ BUILD_PATH=_build
 
 rm -rf $INSTALL_PATH $BUILD_PATH
 
+set -e
+
 cmake -H. -B$BUILD_PATH/Debug -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_BUILD_TYPE=Debug
 cmake -H. -B$BUILD_PATH/Release -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_BUILD_TYPE=Release
 cmake --build $BUILD_PATH/Debug --target install
