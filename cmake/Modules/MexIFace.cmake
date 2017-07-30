3e2baf148ad51e012452629ab947639fb053e926
Mark J. Olah <markjolah@users.noreply.github.com>
2017-07-30 09:50:39 -0600

    Win64 build updates

--- cmake/Modules/MexIFace.cmake
+++ cmake/Modules/MexIFace.cmake
@@ -11,24 +11,33 @@
 # This is the main module to enable the MexIFace support.  This should be inlcuded by target projects
 #
 
-include(MexIFace-configure) #Configure libraries and compile flags
+#Make warnings from MXE cmake wrapper go away
+if(CMAKE_CROSSCOMPILING)
+    set(CMAKE_POLICY_DEFAULT_CMP0017 NEW)
+    set(CMAKE_POLICY_DEFAULT_CMP0020 NEW)
+endif()
+
+cmake_policy(SET CMP0054 NEW) #Don't derrefernce in if() statements
 
 #Fixup library search paths
 if(UNIX)
     execute_process(COMMAND ${MexIFace_CMAKE_EXECUTABLES_DIR}/gcc-lib-dir.sh OUTPUT_VARIABLE GCC_LIB_PATH)
-    set(FIXUP_LIB_SEARCH_PATH ${CMAKE_SYSTEM_LIBRARY_PATH} ${GCC_LIB_PATH})
+    set(FIXUP_LIB_SEARCH_PATH ${CMAKE_SYSTEM_LIBRARY_PATH} ${GCC_LIB_PATH} ${CMAKE_INSTALL_PREFIX}/lib)
+    list(INSERT CMAKE_LIBRARY_PATH 0 ${CMAKE_INSTALL_PREFIX})
 elseif(WIN32)
-    set(FIXUP_LIB_SEARCH_PATH ${CMAKE_FIND_ROOT_PATH} )
+    set(MXE_W64_ROOT ${CMAKE_FIND_ROOT_PATH}) #Extract MXE root path for w64 system
+    set(FIXUP_LIB_SEARCH_PATH ${MXE_W64_ROOT} ${MXE_W64_ROOT}/bin ${CMAKE_INSTALL_PREFIX}/lib)
 else()
 endif()
-set(FIXUP_LIB_IGNORE_PATH ${MATLAB_LIB_DIR}) # Don't include matlab directories in fixup.  Matlab will provide.
 
+include(MexIFace-configure) #Configure libraries and compile flags
 
 message(STATUS "[MexIFace] FIXUP_LIB_SEARCH_PATH:${FIXUP_LIB_SEARCH_PATH}")
+message(STATUS "[MexIFace] FIXUP_LIB_SYSTEM_PATH:${FIXUP_LIB_SYSTEM_PATH}")
 include(${MexIFace_CMAKE_FUNCTIONS_DIR}/fixup_dependencies.cmake)
 
 #Directory relative to CMAKE_INSTALL_PREFIX that the compiled mex files will be stored
-set(MEX_ARCH_DIR lib/${PROJECT_NAME}/mex/mex.${MATLAB_ARCH}${MexIFace_DEBUG_POSTFIX}) #Install dir for mex files
+set(MEX_ARCH_DIR lib/${PROJECT_NAME}/mex/mex.${MATLAB_ARCH}${MexIFace_DEBUG_POSTFIX}/+${PROJECT_NAME}) #Install dir for mex files
 message(STATUS "[MexIFace] MEX_ARCH_DIR:${MEX_ARCH_DIR}")
 include(${MexIFace_CMAKE_FUNCTIONS_DIR}/make_mex.cmake) 
 
