f89fe16aa0117f3b685fe08402d5477049ed4df3
Mark J. Olah <markjolah@users.noreply.github.com>
2019-02-04 15:24:54 -0700

    Armadillo dependency updates

--- build.debug.sh
+++ build.debug.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+# build.debug.sh
 
 INSTALL_PATH=_install
 BUILD_PATH=_build/Debug
+NUM_PROCS=`grep -c ^processor /proc/cpuinfo`
 
+ARGS="-DCMAKE_INSTALL_PREFIX=$INSTALL_PATH"
+ARGS="${ARGS} -DBUILD_TESTING=On"
+ARGS="${ARGS} -DBUILD_STATIC_LIBS=ON"
+ARGS="${ARGS} -DBUILD_SHARED_LIBS=ON"
+ARGS="${ARGS} -DBUILD_TESTING=On"
+ARGS="${ARGS} -DOPT_INSTALL_TESTING=On"
+ARGS="${ARGS} -DOPT_EXPORT_BUILD_TREE=On"
+
+set -ex
 rm -rf $INSTALL_PATH $BUILD_PATH
-
-set -e
-
-cmake -H. -B$BUILD_PATH -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=1
-VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j8
+cmake -H. -B$BUILD_PATH -DCMAKE_BUILD_TYPE=Debug -Wdev ${ARGS}
+VERBOSE=1 cmake --build $BUILD_PATH --target install -- -j${NUM_PROCS}
